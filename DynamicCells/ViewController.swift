7f08d4c14c5d82bd1217271840ce947cca80e7d1
Kyle <kyle200052@hotmail.com>
2019-12-07 23:04:13 -0500

    Initial Commit

---
+++ DynamicCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCells
+//
+//  Created by Kyle Wilson on 2019-12-07.
+//  Copyright © 2019 Xcode Tips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
